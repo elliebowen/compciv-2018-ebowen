246b0d2cc7015201f4444aa155d894a4f30a183f
Ellie Bowen <elliebowen@DN0a223307.SUNet>
2018-02-13 09:28:24 -0800

    homework

--- week-05/sortsequences/tests/sort_numbers.py
+++ week-05/sortsequences/tests/sort_numbers.py
@@ -9,12 +9,12 @@ def numerical_order():
     return sorted(NUMBER_LIST)
 
 def as_absolute_value():
-    def fooabs(x):
+    def fooabs(a):
         return abs(x)
     return sorted(NUMBER_LIST, key = fooabs) 
 
 def as_inverse_number():
-    def fooinv(x):
-        return(1/x)
-    return sorted(NUMBER_LIST, key= fooinv)
+    def fooinv(a)
+    inums = [(1/x) for x in NUMBER_LIST]
+    return sorted(inums)
     
